5963be3889247b4ea758e5f26c1ae2439be2ba11
YJJ <1139953425@qq.com>
2023-04-09 20:26:14 +0800

    test 6

--- app/src/main/java/cn/edu/jnu/x2020101339/Game.java
+++ app/src/main/java/cn/edu/jnu/x2020101339/Game.java
@@ -11,14 +11,33 @@ public class Game {
     public int score() {
         int total=0;
         int current=0;
-        for(int j = 0; j<10; j++){
-            if(rolls[current]+rolls[current+1]==10){
-                total=10+rolls[current+1];
+        while(true){
+            if(isaBoolean(current)){
+                total=10+rolls[current+2]+total;
+                current=current+2;
             }
-            else if(rolls[current]==10){
-                total=10+rolls[current+1]+rolls[current+2];
+            else if(isstrike(current)){
+                total=10+rolls[current+1]+rolls[current+2]+total;
+                current=current++;
             }
+            else{
+                total=rolls[current]+rolls[current+1]+total;
+                current=current+2;
+            }
+            if(isend(current)){break;}
         }
         return total;
+    }
+
+    private boolean isend(int current) {
+        return current > 19;
+    }
+
+    private boolean isstrike(int current) {
+        return rolls[current] == 10;
+    }
+
+    private boolean isaBoolean(int current) {
+        return rolls[current] + rolls[current + 1] == 10;
     }
 }
